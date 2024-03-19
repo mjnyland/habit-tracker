c098439261dee4753a677b62d53c590558a7ea32
Michael <88445485+mjnyland@users.noreply.github.com>
2024-03-19 18:37:33 -0400

    First Commit

---
+++ Habit Tracker/Habit_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackerApp.swift
+//  Habit Tracker
+//
+//  Created by Michael Nyland on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
